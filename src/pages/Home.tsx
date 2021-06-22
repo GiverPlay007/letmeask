757647e1d6f66ae29ced611535bf8595ac2c828b
GiverPlay <play.giver.007@gmail.com>
2021-06-21 22:07:28 -0300

    Firebase auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../services/firebase";
 import { Button } from "../components/Button";
 
 import illustrationImage from "../assets/images/illustration.svg";
@@ -10,8 +11,13 @@ import "../styles/auth.scss";
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push("/rooms/new");
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+      history.push("/rooms/new");
+    });
   }
 
   return(
@@ -24,7 +30,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImage} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImage} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
