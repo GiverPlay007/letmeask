2c7b4ed41c02ee071a91ed32e51b2eb3e4f9ec62
GiverPlay007 <play.giver.007@gmail.com>
2021-06-22 22:44:06 -0300

    Room page main layout

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,35 @@
+import { Button } from "../components/Button";
+import logoImage from "../assets/images/logo.svg";
+
+import "../styles/room.scss";
+
 export function Room() {
   return(
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImage} alt="Letmeask" />
+          <div>Código</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea
+            placeholder="O que você quer perguntar?" 
+          />
+
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
