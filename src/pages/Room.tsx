d0613073c851f1902c8af7e080499e2dd1129339
GiverPlay007 <play.giver.007@gmail.com>
2021-06-22 22:59:38 -0300

    Create room code component

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,15 +1,24 @@
+import { RoomCode } from "../components/RoomCode";
+import { useParams } from "react-router-dom";
 import { Button } from "../components/Button";
+
 import logoImage from "../assets/images/logo.svg";
 
 import "../styles/room.scss";
 
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return(
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImage} alt="Letmeask" />
-          <div>Código</div>
+          <RoomCode code={params.id} />
         </div>
       </header>
 
