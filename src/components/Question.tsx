d7c0e88cc9fe7695c51e951ccfe5957093f0d146
GiverPlay007 <play.giver.007@gmail.com>
2021-06-23 21:29:04 -0300

    Like visual

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 import "../styles/question.scss";
 
 type QuestionProps = {
@@ -5,10 +7,11 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-  }
+  },
+  children?: ReactNode;
 };
 
-export function Question({ content, author }: QuestionProps) {
+export function Question({ content, author, children }: QuestionProps) {
   return (
     <div className="question">
       <p>{content}</p>
@@ -17,7 +20,7 @@ export function Question({ content, author }: QuestionProps) {
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>{children}</div>
       </footer>
     </div>
   );
